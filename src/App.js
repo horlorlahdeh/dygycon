6274b5d46cdf6f49222b5b06ba93d6ddaad7c4dd
Ololade Isreal <horlorlahdeh@gmail.com>
2020-10-07 20:57:30 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/index';
+import About from './pages/About/index';
+import Pricing from './pages/Pricing/index';
+import Support from './pages/Support/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Router>
+        <Route exact path='/' component={Home} />
+        <Switch>
+          <Route exact path='/pricing' component={Pricing} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/support' component={Support} />
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
