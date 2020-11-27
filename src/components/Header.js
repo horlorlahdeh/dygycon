5752b5932e2f8cb7b1c2ddbac706021cd67af363
Ololade Isreal <horlorlahdeh@gmail.com>
2020-11-27 23:55:53 +0100

    time fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
             </div>
             <div className='row'>
               <div className='col-md-12 flex'>
-                <Countdown targetDate='Nov 28, 2020' targetTime='15:00:00' />
+                <Countdown targetDate='Nov 28, 2020' targetTime='10:55:00' />
               </div>
             </div>
             <div className='row text-center mt-3'>
